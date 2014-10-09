35b7dca7e3849b590624642ace0808a965ac9b3a
Steven <steven@scorbord.com>
2014-10-09 08:40:53 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScorbordV0::Application.config.secret_key_base = '3e887ec015274cbdddec0d6a0ec3d03188b8729ea7f67305be2c0f1898d1df6e71e6c64db5b3b16aa358db1e973acb6c1f760e50ea800966fb98f107ec123ca7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScorbordV0::Application.config.session_store :cookie_store, key: '_scorbord_v0_session'
