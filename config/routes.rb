ba75d7fbd820c854866248018076638569f633ab
Steven <steven@scorbord.com>
2014-10-18 16:11:18 -0400

    adding the leftovers?

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 ScorbordV0::Application.routes.draw do
+<<<<<<< HEAD
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
+=======
+>>>>>>> filling-in-layout
   
   resources :drives
 
   resources :plays
 
   resources :users
+
+  root to: 'static_pages#home',  via: [:get, :post]
+
+  match '/help',    to: 'static_pages#help',     via: [:get, :post]
+  match '/about',   to: 'static_pages#about',    via: [:get, :post]
+  match '/contact', to: 'static_pages#contact',  via: [:get, :post]
+
+  match '/signup',  to: 'users#new',             via: [:get, :post]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,45 +2,43 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the content 'scRORbord'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'scRORbord')
-    end
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                              :text => " | Home")
-    end
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',        text: 'scRORbord') }
+    
+    it { should have_selector('title',     text: full_title('')) }
+
+    it { should_not have_selector 'title', text: '| Home' }
+
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-  	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('h1', :text => 'Help')
-  	end
+  	it { should have_selector('h1',        text: 'Help') }
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                              :text => " | Help")
-    end
+    it { should have_selector('title',     text: full_title('Help')) }
+    
   end
 
   describe "About page" do
+    before { visit about_path }
 
-  	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1', :text => 'About Us')
-  	end
+  	it { should have_selector('h1',        text: 'About') }
+  	
+    it { should have_selector('title',     text: full_title('About')) }
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                              :text => " | About Us")
-    end
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',        text: 'Contact') }
+
+    it { should have_selector('title',     text: full_title('Contact')) }
 
   end
 
