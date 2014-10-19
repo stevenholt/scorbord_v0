785e507339f1a91fe1e75380707501d9facae156
Steven <steven@scorbord.com>
2014-10-19 06:56:49 -0400

    fixed specs

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,16 +1,35 @@
 require 'spec_helper'
 
-describe "User pages" do
-	
-	subject { page }
+describe "Static pages" do
 
-	describe "signup page" do
-		before { visit signup_path }
+  subject { page }
 
-		it { should have_selector('h1',			text: 'Sign up') }
+  describe "Home page" do
+    before { visit root_path }
 
-		it { should have_selector('title',		text: full_title('Sign up')) }
+    it { should have_selector('h1',    text: 'scRORbord') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
 
-	end
+  describe "Help page" do
+    before { visit help_path }
 
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
