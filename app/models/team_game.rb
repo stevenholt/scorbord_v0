49c0571f68fa3b34680952c4979c4546d5689d65
Steven <steven@scorbord.com>
2014-10-20 19:20:43 -0400

    goofing around, added TeamGame

--- app/models/team_game.rb
+++ app/models/team_game.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: team_games
+#
+#  id         :integer          not null, primary key
+#  team_id    :integer
+#  game_id    :integer
+#  home_team  :boolean
+#  away_team  :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class TeamGame < ActiveRecord::Base
 	belongs_to :team
 	belongs_to :game
--- spec/models/team_game_spec.rb
+++ spec/models/team_game_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: team_games
+#
+#  id         :integer          not null, primary key
+#  team_id    :integer
+#  game_id    :integer
+#  home_team  :boolean
+#  away_team  :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe TeamGame do
